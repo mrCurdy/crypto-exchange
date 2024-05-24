16a34bdaa045592101892ef7d1dee924b22b9731
DimS <mr.curdy@gmail.com>
2024-05-24 19:59:35 +0300

    more changes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Header from "./Header";
+import Body from "./Body";
+import React from "react";
+
+function App() {
+  const [showSideBar, setShowSideBar] = React.useState(false);
+  return (
+    <Container>
+      <Header setShowSideBar={setShowSideBar} />
+      <Body showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
+    </Container>
+  );
+}
+
+export default App;
