162a3702a99af71c1e7635dab6570d4beb5dac72
DimS <mr.curdy@gmail.com>
2024-05-31 15:03:01 +0300

    charts axis x and y formated

--- src/Body/CoinInfo/Chart.jsx
+++ src/Body/CoinInfo/Chart.jsx
@@ -25,7 +25,7 @@ function Chart({ chartData, priceMax, priceMin }) {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        {/* вставили форматор даты в XAxis
+        {/* вставили форматор даты в XAxis..
         решение нашёл тут: https://codesandbox.io/p/sandbox/recharts-area-chart-with-date-axis-6o55k?file=%2Fsrc%2FDateArea.js%3A2%2C66-2%2C74 */}
         <XAxis dataKey="date" tickFormatter={dateFormatter} />
         <YAxis
